6b91f7b3ff55a7125ea38da593a82d94cc769b93
Lchiffon <langdawei@shu.edu.cn>
2014-04-16 10:09:26 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+
+test<-function(x){
+  ## it is a test function showing that 
+  ## the symbol "<<-" is give the value to
+  ## a variance in global Environment instead of 
+  ## current enviroment
+  a<<-x^2
+  ## give a value to a in global environment
+  x 
+}
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     
+  m <- y$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- y$get()
+  m <- solve(data, ...)
+  y$setinverse(m)
+  m     ## Return a matrix that is the inverse of 'x'
 }
